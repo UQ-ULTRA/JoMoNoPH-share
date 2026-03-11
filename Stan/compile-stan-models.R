ab884ea0352052aac3fa81cca340077477163eb3
Ding Ma <48374197+ding-ma1@users.noreply.github.com>
2026-03-11 16:34:47 +1000

    Update

--- Stan/compile-stan-models.R
+++ Stan/compile-stan-models.R
@@ -13,10 +13,30 @@ set_cmdstan_path("~/.cmdstan/cmdstan-2.37.0")
 
 
 # compile and save the models
-cmdstan_model("~/JoMoNoPH-share/Stan/Bernstein-Polynomials-JM-Hist.stan",
+# cmdstan_model("~/JoMoNoPH-share/Stan/Bernstein-Polynomials-JM-Hist.stan",
+#               cpp_options = list(stan_threads = TRUE),
+#               force_recompile = TRUE)
+# 
+# cmdstan_model("~/JoMoNoPH-share/Stan/Gaussian-Basis-JM-Hist.stan",
+#               cpp_options = list(stan_threads = TRUE),
+#               force_recompile = TRUE)
+
+cmdstan_model("~/JoMoNoPH-share/Stan/Bernstein-Polynomials.stan",
               cpp_options = list(stan_threads = TRUE),
               force_recompile = TRUE)
 
-cmdstan_model("~/JoMoNoPH-share/Stan/Gaussian-Basis-JM-Hist.stan",
+cmdstan_model("~/JoMoNoPH-share/Stan/Gaussian-Basis.stan",
+              cpp_options = list(stan_threads = TRUE),
+              force_recompile = TRUE)
+
+cmdstan_model("~/JoMoNoPH-share/Stan/Gaussian-Basis-logy.stan",
+              cpp_options = list(stan_threads = TRUE),
+              force_recompile = TRUE)
+
+cmdstan_model("~/JoMoNoPH-share/Stan/Gaussian-Basis-logy-adaptive-grid.stan",
+              cpp_options = list(stan_threads = TRUE),
+              force_recompile = TRUE)
+
+cmdstan_model("~/JoMoNoPH-share/Stan/Bernstein-Polynomials-alt.stan",
               cpp_options = list(stan_threads = TRUE),
               force_recompile = TRUE)
