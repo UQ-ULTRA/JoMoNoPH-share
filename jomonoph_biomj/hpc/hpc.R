69f1b9154015315aa5f79290f91e729c5634a4df
Ding Ma <48374197+ding-ma1@users.noreply.github.com>
2026-04-14 15:12:04 +1000

    propose a folder structure for replication

--- jomonoph_biomj/hpc/hpc.R
+++ jomonoph_biomj/hpc/hpc.R
@@ -1,7 +1,7 @@
 1.read relevant simulation scenario from scenario.R
 2.generate data by calling datagen.R
 3.fit model by calling stan models
-4.save result
+4.save results in /hpc/output_yyyymmddhhmm folder in a specific format (e.g. .rds) for later aggregation 
 5.aggregate results by calling aggregate_hpc.R
 6.produce tables
 7.produce figures by calling plot_hpc.R
